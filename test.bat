fe0d477d5c0cef8f2bfa3219efb8f790671bb599
needleful <needleful@outlook.com>
2025-01-23 02:22:44 -0800

    Generics working better

--- test.bat
+++ test.bat
@@ -3,6 +3,6 @@ swipl -s cpp.pl -- .\examples\01_functions.c+p .\output\01_functions.c
 swipl -s cpp.pl -- .\examples\02_structs.c+p .\output\02_structs.c
 swipl -s cpp.pl -- .\examples\03_parser.c+p .\output\03_parser.c
 swipl -s cpp.pl -- .\examples\04_macros.c+p .\output\04_macros.c
-swipl -s cpp.pl -- .\examples\05a_generics.c+p .\output\05a_generics.c
+swipl -s cpp.pl -- .\examples\05_generics.c+p .\output\05_generics.c
 swipl -s cpp.pl -- .\examples\06_reflection.c+p .\output\06_reflection.c
 swipl -s cpp.pl -- .\examples\07_directives.c+p .\output\07_directives.c
